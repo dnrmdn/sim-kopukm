08a0ce697631c632e2e85c4a424e66fd9445631c
rofhzose <96776439+rofhzose@users.noreply.github.com>
2026-03-06 23:12:44 +0700

    OK NIH

--- backend/server.js
+++ backend/server.js
@@ -207,7 +207,12 @@ app.use("/api/jabatan", jabatanRouter);
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 app.use("/api/rka", rkaRouter);
+=======
+app.use("/api/pegawai-hirarki", pegawaiHirarkiRoutes);
+app.use("/api/rka", rkaRoutes);
+>>>>>>> Stashed changes
 =======
 app.use("/api/pegawai-hirarki", pegawaiHirarkiRoutes);
 app.use("/api/rka", rkaRoutes);
